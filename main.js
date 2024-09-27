aa264affcf05cb91f11995b323314044adf72601
Gamzat <104684754+RGamz@users.noreply.github.com>
2024-09-27 16:08:38 +0200

    feat: small update to adapt window behavior ✨

--- main.js
+++ main.js
@@ -10,5 +10,17 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
-  createWindow();
-});
+  createWindow()
+
+  // macOS apps generally continue running even without any windows open. 
+  // Activating the app when no windows are available should open a new one.
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
