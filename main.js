f7ff4b18149244fe367e49949a4e02464444ad00
Gamzat <104684754+RGamz@users.noreply.github.com>
2024-10-02 20:29:21 +0200

    Frontend update  (#4)
    feat: add window for write text (new styles) ✨
    @RGamz

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
-const { app, BrowserWindow } = require("electron");
+import { app, BrowserWindow, screen } from "electron";
+import dotenv from 'dotenv';
+
+// load env variables
+dotenv.config();
 
 const createWindow = () => {
+
+  // Get the primary display (the main screen the app will open on)
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  // Set width and height as percentages of the screen's size
+  const winWidth = Math.floor(width * 0.8); // 80% of screen width
+  const winHeight = Math.floor(height * 0.8); // 80% of screen height
+
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: winWidth,
+    height: winHeight,
+    webPreferences: {
+      nodeIntegration: true,
+      zoomFactor: 1
+    },
+    resizable: true, // Ensures the window can be resized
+    fullscreenable: true, // Allow fullscreen
   });
+
+  // Only open DevTools in local environment!
+  if (process.env.APP_ENV === 'local') {
+    win.webContents.openDevTools(); 
+  }
+
+  win.setMinimumSize(200, 200)
 
   win.loadFile("index.html");
 };
@@ -17,10 +42,9 @@ app.whenReady().then(() => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+});
 
 // Quit the app when all windows are closed (Windows & Linux)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
-})
-
+});
