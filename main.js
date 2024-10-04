b8395520603bcd657d5cee59913fd9b090ad8701
Ilia Chubarov <agoalofalife@gmail.com>
2024-10-04 17:06:33 +0400

    change createWindow function and return window object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { app, BrowserWindow, screen, Tray, Menu, nativeImage } from "electron";
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import dotenv from 'dotenv';
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import dotenv from "dotenv";
 
 // load env variables
 dotenv.config();
@@ -11,84 +11,88 @@ const createWindow = () => {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
   // Set width and height as percentages of the screen's size
-  const winWidth = Math.floor(width * 0.8); // 80% of screen width
-  const winHeight = Math.floor(height * 0.8); // 80% of screen height
+  const windowWidth = Math.floor(width * 0.8); // 80% of screen width
+  const windowhHeight = Math.floor(height * 0.8); // 80% of screen height
 
-  //removed const to avoid conflicts and add let win upper 
-  const window = new BrowserWindow({
-    width: winWidth,
-    height: winHeight,
+  return new BrowserWindow({
+    width: windowWidth,
+    height: windowhHeight,
     webPreferences: {
       nodeIntegration: true,
-      zoomFactor: 1
+      zoomFactor: 1,
     },
     resizable: true, // Ensures the window can be resized
     fullscreenable: true, // Allow fullscreen
   });
+};
+
+app.whenReady().then(() => {
+  const window = createWindow();
 
   // Only open DevTools in local environment!
-  if (process.env.APP_ENV === 'local') {
-    window.webContents.openDevTools(); 
+  if (process.env.APP_ENV === "local") {
+    window.webContents.openDevTools();
   }
 
-  window.setMinimumSize(200, 200)
+  window.setMinimumSize(200, 200);
 
   window.loadFile("index.html");
 
   //add event for hide and close app
-  window.on('close', (event) => {
+  window.on("close", (event) => {
     if (!app.isQuitting) {
       event.preventDefault();
       window.hide();
     }
   });
-  
+
   // Add icon for Tray bar
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   // for examle add patch for icon, it will be fixed. Check dir 'icons'
-  const iconPath = path.join(__dirname, 'icons', 'frogIcon.png');
+  const iconPath = path.join(__dirname, "icons", "patch.ico");
   const icon = nativeImage.createFromPath(iconPath);
-  icon.resize({width: 16, height:16});
+  icon.resize({ width: 16, height: 16 });
   let tray = new Tray(icon);
 
   //Add function hide and show when y double click on ison WITHOUT contextMenu
   //I dont know how but with "CLICK" ITS WORK BUT WITH "DOUBLE-CLICK" NOT WORK, mb conflict with contextMenu?
-  tray.on('click', () => {
+  tray.on("click", () => {
     window.isVisible() ? window.hide() : window.show();
   });
-  //Add cont menu for click on Tray bar icon 
+
+  //Add cont menu for click on Tray bar icon
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Open Frog-app', type: 'normal', 
-        click: () => {
-          window.show();
-        }
+    {
+      label: "Open Frog-app",
+      type: "normal",
+      click: () => {
+        window.show();
       },
-      { label: 'Options', type: 'normal' },        //if it will be? + add event
-      { label: 'Close Frog-app', type: 'normal',
-        //Add event for button 'Close'
-        click: () => {
-          app.isQuitting = true;
-          window.close();
-        }
+    },
+    { label: "Options", type: "normal" }, //if it will be? + add event
+    {
+      label: "Close Frog-app",
+      type: "normal",
+      //Add event for button 'Close'
+      click: () => {
+        app.isQuitting = true;
+        window.close();
       },
-    ])
+    },
+  ]);
 
-    tray.setToolTip('Frog-app.');          //NEED TO TEST
-    tray.setContextMenu(contextMenu);
-};
-
-app.whenReady().then(() => {
-    createWindow()
+  tray.setToolTip("Frog-app.");
+  tray.setContextMenu(contextMenu);
 });
 
-// macOS apps generally continue running even without any windows open. 
+// macOS apps generally continue running even without any windows open.
 // Activating the app when no windows are available should open a new one.
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 //Quit the app when all windows are closed (Windows & Linux)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
