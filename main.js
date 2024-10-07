c5b1fb3807d46f4c74aae3a23d893a3d8ebc0aea
Ilia Chubarov <agoalofalife@gmail.com>
2024-10-07 21:10:51 +0400

    add .idea in gitignore file and fix clicks by icon and context menu pop up

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, screen, Tray, Menu, nativeImage, nativeTheme } from "electron";
+import { app, BrowserWindow, screen, Tray, Menu, nativeImage } from "electron";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import dotenv from "dotenv";
@@ -12,11 +12,11 @@ const createWindow = () => {
 
   // Set width and height as percentages of the screen's size
   const windowWidth = Math.floor(width * 0.8); // 80% of screen width
-  const windowhHeight = Math.floor(height * 0.8); // 80% of screen height
+  const windowHeight = Math.floor(height * 0.8); // 80% of screen height
 
   return new BrowserWindow({
     width: windowWidth,
-    height: windowhHeight,
+    height: windowHeight,
     webPreferences: {
       nodeIntegration: true,
       zoomFactor: 1,
@@ -49,43 +49,34 @@ app.whenReady().then(() => {
   // Add icon for Tray bar
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
+
   /*use template images for a menu bar (Tray) icon, so it can adapt to both light and dark menu bars.
-  On platforms that support high pixel density displays (such as Apple Retina), we can append @2x after image's base 
+  On platforms that support high pixel density displays (such as Apple Retina), we can append @2x after image's base
   filename to mark it as a 2x scale high resolution image. */
   const iconPath = path.join(__dirname, "icons", "frogIconTemplate.png");
   const icon = nativeImage.createFromPath(iconPath);
   icon.resize({ width: 16, height: 16 });
   let tray = new Tray(icon);
 
-  //Add function hide and show when y double click on ison WITHOUT contextMenu
-  //I dont know how but with "CLICK" ITS WORK BUT WITH "DOUBLE-CLICK" NOT WORK, mb conflict with contextMenu?
-  tray.on("click", () => {
+  tray.on('click', () => {
     window.isVisible() ? window.hide() : window.show();
-  });
-
+    tray.closeContextMenu();
+  })
+  tray.on('right-click', () => {
+    tray.popUpContextMenu(contextMenu)
+  })
   //Add cont menu for click on Tray bar icon
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: "Open Frog-app",
-      type: "normal",
-      click: () => {
-        window.show();
-      },
-    },
-    { label: "Options", type: "normal" }, //if it will be? + add event
-    {
       label: "Close Frog-app",
       type: "normal",
-      //Add event for button 'Close'
       click: () => {
-        app.isQuitting = true;
-        window.close();
+        app.exit(0);
       },
     },
   ]);
 
   tray.setToolTip("Frog-app.");
-  tray.setContextMenu(contextMenu);
 });
 
 // macOS apps generally continue running even without any windows open.
