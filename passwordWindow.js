51a920a5c8a99daf224051f72a1b9d190b171aac
RGamz <rgamzat@live.com>
2024-10-15 20:02:12 +0200

    fix master password id

--- passwordWindow.js
+++ passwordWindow.js
@@ -1,6 +1,6 @@
 document.querySelector('.passwordWindowForm').addEventListener('submit', (e) => {
     e.preventDefault();
-    const password = document.getElementById('masterPasswordInputField').value;
+    const password = document.getElementById('masterPassword').value;
     // Use the exposed API to send the password
     window.electronAPI.submitPassword(password);
   });
