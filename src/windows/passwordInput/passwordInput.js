1c698ce279e8199b3038a3b75b3c94932c718aa2
Toxa-K <originkewai666.com@mail.ru>
2024-12-20 20:14:32 +0300

    MicroFixForFix#2

--- src/windows/passwordInput/passwordInput.js
+++ src/windows/passwordInput/passwordInput.js
@@ -6,14 +6,6 @@ document.getElementById('passwordInputForm').addEventListener('submit', (e) => {
     window.electronAPI.verifyPassword(enteredPassword);
 });
 
-window.electronAPI.onFillPasswordField((password) => {
-    const passwordInputField = document.getElementById('passwordInputField');
-    if (passwordInputField) {
-        passwordInputField.value = password;
-        document.getElementById('passwordInputForm').submit();
-    }
-});
-
 // Handle incorrect password feedback
 window.electronAPI.onPasswordIncorrect(() => {
     alert('Incorrect password. Please try again.');
