2ba01982064ccbde8f2835a68d714a4c79ceb226
RGamz <rgamzat@live.com>
2025-01-06 20:40:22 +0100

    feat: correct setup

--- src/js/renderer.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("saveAndEncryptButton").addEventListener("click", (event) => {
-    const textInApp = document.getElementById("textToEncrypt").value;
-    window.electronAPI.sendAndEncrypt(textInApp);
-  });
-
-// call method form preload.js and install text from textarea in app
-window.electronAPI.loadText().then(text => {
-  document.getElementById("textToEncrypt").value = text;
-});
--- src/js/toggleVisibility.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('togglePasswordCheckbox').addEventListener('change', function () {
-    var x = document.getElementById("passwordInputField");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-});
