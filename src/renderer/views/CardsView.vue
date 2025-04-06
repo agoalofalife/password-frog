e56ef2373712ac3608c44b06ae641f32ee2f277b
Toxa-K <originkewai666.com@mail.ru>
2025-04-06 21:03:07 +0300

    Copy CardDesign branch

---
+++ src/renderer/views/CardsView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <div class="searchbar">
+      <Searchbar></Searchbar>
+    </div>
+
+    <div class="cards_window">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import Card from "../components/Card.vue";
+import Searchbar from "../components/Searchbar.vue";
+
+onMounted(async () => {
+  //pass
+});
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.searchbar {
+  margin-bottom: 2vh;
+}
+
+.cards_window {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 3vw;
+}
+</style>
